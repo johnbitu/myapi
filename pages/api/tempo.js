b059a88782577f7e645e451f2af1fd4760e714be
JohnBitu <bitujoaovictor@gmail.com>
2024-01-29 16:38:24 -0300

    adicionando integração com a API do PokeAPI, puxando uma imagem de um pokémon aleatório.

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,18 @@
 async function tempo(request, response) {
     const dynamicDate = new Date();
 
+    // Randomizando os pokemons
+    const pokemonId = Math.floor(Math.random() * 905) + 1;
+
+    const imageUrl = 'https://pokeapi.co/api/v2/pokemon/${pokemonId}/sprites';
+
+    const response = await fetch(imageUrl);
+
+    const imageData = response.blob();
+
+    const imageElement = document.createElement("img");
+    imageElement.src = URL.createObjectURL(imageData);
+    document.body.appendChild(imageElement);
 
     response.json({
         data: dynamicDate.toGMTString()
