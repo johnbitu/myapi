d642dbcab84584fabe917ed87a28dff649734417
JohnBitu <bitujoaovictor@gmail.com>
2024-01-31 09:08:37 -0300

    .

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,25 +1,42 @@
-async function tempo(request, response) {
+// tempo.js
+
+import fetch from 'node-fetch';
+
+export default async function tempo(request, response) {
     const dynamicDate = new Date();
 
     // Randomizando os pokemons
     const pokemonId = Math.floor(Math.random() * 905) + 1;
     console.log(pokemonId);
 
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-    const pokeData = await pokemon.json();
-    const pokeNome = pokeData.name;
+    try {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
 
-
-    response.json({
-        data: dynamicDate.toGMTString(),
-        pokemon: {
-            pokemonId: pokemonId,
-            name: pokeNome
+        if (!pokemon.ok) {
+            throw new Error(`Erro na requisição: ${pokemon.status} ${pokemon.statusText}`);
         }
-    })
 
-    // Atualiza a página automaticamente em 5 segundos
+        const pokeData = await pokemon.json();
+        const pokeNome = pokeData.name;
 
+        response.json({
+            data: dynamicDate.toGMTString(),
+            pokemon: {
+                pokemonId: pokemonId,
+                name: pokeNome
+            }
+        });
+    } catch (error) {
+        console.error('Erro ao acessar a API:', error.message);
+        response.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+    // Agende a próxima atualização após 3 segundos (3000 milissegundos)
+    setTimeout(() => atualizarAPI(response), 3000);
 }
 
-export default tempo;
+function atualizarAPI(response) {
+    // Faça a chamada à API aqui
+    tempo(null, response);
+    console.log('API atualizada!');
+}
