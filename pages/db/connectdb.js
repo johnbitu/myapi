66085d3e0af7feb9553e192d7cbf241bfa1654e8
JohnBitu <bitujoaovictor@gmail.com>
2024-02-05 17:30:36 -0300

    adicionando o login e tentando concertar a validação de login

--- pages/db/connectdb.js
+++ pages/db/connectdb.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5500;
@@ -17,11 +20,18 @@ const db = new sqlite3.Database('database.db');
 // `);
 
 // Middleware para parsear o corpo das requisições como JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/cadastro', (req, res) => {
   res.sendFile(__dirname + '/cadastro.html');
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
 
 app.get('/users', (req, res) => {
   db.all('SELECT * FROM users', (err, rows) => {
@@ -33,19 +43,54 @@ app.get('/users', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  const { name, email } = req.body;
+  const { name, email, senha } = req.body;
 
-  console.log('Received data:', name, email);
+  console.log('Received data:', name, email, senha);
   // if (!name || !email) {
   //   return res.status(400).json({ error: 'Name and email are required' });
   // }
 
-  db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email ], function (err) {
+  db.run('INSERT INTO users (name, email, senha) VALUES (?, ?, ?)', [name, email, senha], function (err) {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
 
     res.redirect('/');
+  });
+});
+
+// Rota de login
+app.post('/login', async (req, res) => {
+  const { name, senha } = req.body;
+  // name = "'" + name + "'"
+  // console.log(name,senha);
+
+  // Consultar o banco de dados para obter o hash da senha do usuário
+  db.get(`SELECT * FROM users WHERE LOWER(name) = LOWER(?)`, [name], async (err, row) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send('Erro interno do servidor');
+    }
+
+    if (row) {
+      // Comparar a senha fornecida com o hash no banco de dados
+      const passwordMatch = await bcrypt.compare(senha, row.senha);
+
+      if (passwordMatch) {
+        // Usuário autenticado com sucesso
+        console.log(name, senha, "if 1");
+        return res.status(200).json({ message: 'Login efetuado com sucesso' });
+      } else {
+        // Senha incorreta
+        console.log(name, senha, "else 1");
+        return res.status(401).send('Credenciais inválidas');
+      }
+    } else {
+      console.log(name, senha, "else 2");
+      // Usuário não encontrado
+      return res.status(401).send('Credenciais inválidas');
+
+    }
   });
 });
 
